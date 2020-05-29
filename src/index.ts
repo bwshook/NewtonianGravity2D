d356f25b08e4ba505e619d78745e7abdc885f3ee
Brett Shook <brett@Bretts-MacBook-Pro.local>
2020-05-29 01:05:45 -0700

    kepler solution

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,20 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // 3. Using Verlet integration
 
 let aspectRatio = window.innerWidth / window.innerHeight
-let camera = new THREE.PerspectiveCamera(60, aspectRatio, 0.1, 1000);
+let camera = new THREE.PerspectiveCamera(45, aspectRatio, 0.1, 1000);
 let scene = new THREE.Scene();
 let renderer = new THREE.WebGLRenderer({ antialias: true });
 
-let geometry = new THREE.SphereGeometry(1, 8, 6);
+let geometry = new THREE.SphereGeometry(1, 16, 16);
 let material = new THREE.MeshNormalMaterial();
 let body1 = new THREE.Mesh(geometry, material);
 let body2 = new THREE.Mesh(geometry, material);
+let grid = new THREE.GridHelper(10, 10);
 
 let controls = new OrbitControls(camera, renderer.domElement);
 let stats = Stats();
+
+let phi = 0.0;
 
 init();
 animate();
@@ -28,11 +31,12 @@ function init() {
     document.body.appendChild(container);
     container.appendChild(stats.dom);
 
-    camera.position.set(0, 0, 5);
+    camera.position.set(0, 0, 10);
     controls.update();
 
     scene.add(body1);
     scene.add(body2);
+    scene.add(grid);
 
     renderer.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(renderer.domElement);
@@ -45,15 +49,29 @@ function OnWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+function kepler(m1:number, m2:number, rmin:number, vmin:number, phi:number) {
+  // Solution to the Kepler Problem
+  let u = (m1*m2)/(m1+m2); // Reduced Mass
+  let G = 6.67430e-11; // Grav. Constant
+  G = 1;
+  let l = rmin*u*vmin; // Angular momentum
+  let g = G*m1*m2; // Gamma
+  let c = (l*l)/(g*u); //
+  let A = 1/rmin - 1/c; // Oribital magnitude
+  let e = A*c; // Eccentricity
+  return( c/(1+e*Math.cos(phi)) );
+}
+
+
 function animate() {
     let d = new Date();
     let t = d.getMilliseconds()*0.001;
-    let w = 2.0*Math.PI;
-    let phi = w*t;
-    let sinPos = Math.sin(phi);
-    let cosPos = Math.cos(phi);
-    body1.position.set(sinPos, cosPos, 0);
-    body2.position.set(cosPos, 0, 0);
+    body1.position.set(0, 0, 0);
+    phi += 0.01;
+    let r = kepler(10, 1, 4, 2, phi);
+    let x = r*Math.cos(phi);
+    let y = r*Math.sin(phi);
+    body2.position.set(x, 0, y);
 
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
